bce76de2e5e3120202817f90cad5a954a603fcb1
Ananthu <ananthu@Ananthus-MacBook-Air.local>
2023-06-30 16:07:08 +0530

    third

--- index.js
+++ index.js
@@ -9,21 +9,67 @@ const db = firebase.database();
 const app=express();
 let readings={};
 
-function read_sensor()
-{
-  const readingsRef = firebase.database().ref('DHT/readings');
-    // Read the data from the database
+function read_sensor() {
+  return new Promise((resolve, reject) => {
+    const readingsRef = firebase.database().ref('DHT/readings');
+
     readingsRef.once('value')
-    .then((snapshot) => {
-      // The data snapshot contains the data from the "DHT/readings" node
-      readings = snapshot.val();
-      console.log("inside fn",readings);
-      return readings;
-    })
-    .catch((error) => {
-      console.error('Error reading data:', error);
-    });
-    return readings;
+      .then((snapshot) => {
+        const allReadings = snapshot.val();
+        const readingsArray = Object.values(allReadings);
+        const lastThreeReadings = readingsArray.slice(-3);
+
+        // Calculate the average of all three values
+        const averageReading = calculateAverage(lastThreeReadings);
+
+        const averageObj = {
+          humidity: averageReading.humidity,
+          moisture: averageReading.moisture,
+          temperature: averageReading.temperature
+        };
+
+        resolve(averageObj);
+      })
+      .catch((error) => {
+        console.error('Error reading data:', error);
+        reject(error);
+      });
+  });
+}
+
+function calculateAverage(readings) {
+  if (readings.length === 0) {
+    return { humidity: 0, moisture: 0, temperature: 0 }; // Return an object with 0 values if there are no readings
+  }
+
+  const sum = readings.reduce((acc, reading) => {
+    return {
+      humidity: acc.humidity + reading.humidity,
+      moisture: acc.moisture + reading.moisture,
+      temperature: acc.temperature + reading.temperature
+    };
+  }, { humidity: 0, moisture: 0, temperature: 0 });
+
+  const average = {
+    humidity: sum.humidity / readings.length,
+    moisture: sum.moisture / readings.length,
+    temperature: sum.temperature / readings.length
+  };
+
+  return average;
+}
+
+async function readCropData() {
+  const ref = db.ref('crops'); // Replace 'path/to/data' with your actual database path
+
+  try {
+    const snapshot = await ref.once('value');
+    const data = snapshot.val();
+    const readingsArray = Object.values(data);
+    return readingsArray;
+  } catch (error) {
+    throw new Error('Error reading data: ' + error);
+  }
 }
 
 app.use(express.static("public"));
@@ -52,12 +98,20 @@ app.get("/register.html",function(req,res){
     res.sendFile(__dirname+"/register.html");
 })
 
-app.get("/marketplace",function(req,res){
-  res.render("marketplace",{title:'marketplace'});
-})
+app.get("/marketplace", async function(req, res) {
+  try {
+    const reading = await readCropData();
+    console.log("Inside", reading);
+    res.render("marketplace", { title: 'marketplace',crops:reading});
+  } catch (error) {
+    console.log("Error:", error);
+    // Handle the error accordingly
+    res.status(500).send("An error occurred");
+  }
+});
 
-app.listen(5000,function(){
-    console.log("Server started on port 5000");
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
 })
 
 app.post("/register",function(req,res)
@@ -85,32 +139,31 @@ app.post("/register",function(req,res)
 });
 
 app.post("/dashboard", async function(req, res) {
-    const email = req.body.username;
-    const password = req.body.password;
-  
-    try {
-      const usersRef = db.ref("users");
-      usersRef.once("value", function(snapshot) {
-        const users = snapshot.val();
-        const foundUser = Object.values(users).find(
-          user => user.email === email && user.password === password
-        );
-        if (foundUser) {
-          //read_sensor();
-          console.log("Outside",read_sensor());
-          console.log("outside",readings);
-          res.render("dashboard");
-          //res.sendFile(__dirname + "/dashboard.html");
-        } else {
-          // Username or password is incorrect
-          res.status(401).send("Invalid username or password");
-        }
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).send("Internal server error");
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const usersRef = db.ref("users");
+    const snapshot = await usersRef.once("value");
+    const users = snapshot.val();
+    const foundUser = Object.values(users).find(
+      user => user.email === email && user.password === password
+    );
+    if (foundUser) {
+      const readings = await read_sensor();
+      console.log(readings);
+      // Assuming the readings array contains the data you provided
+      //console.log("Outside", readings);
+      res.render("dashboard",{readings:readings});
+    } else {
+      // Username or password is incorrect
+      res.status(401).send("Invalid username or password");
     }
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal server error");
+  }
+});
 
 
   app.post("/add_crop.html",function(req,res){
